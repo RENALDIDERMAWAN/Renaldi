1c00cf6ea757e1251eeb88be16087acc6fc99338
RENALDIDERMAWAN <162908916+RENALDIDERMAWAN@users.noreply.github.com>
2024-06-03 18:43:37 +0700

    Add files via upload

---
+++ tugas-mvc-tutor/app/views/about/index.php
@@ -0,0 +1,9 @@
+<div class="container">
+
+        <h1 class="mt-6">About Me</h1>
+        <img src="<?= BASEURL; ?>/img/slbcicendo.png" alt="Renaldi Dermawan"
+        width = "200" class = "rounded-circle shadow">
+        <p>Hallo, Selamat Datang <?= $data['nama']; ?>, Sekolah Kami <?= $data['slogan']; ?>.
+        </p>
+
+</div>
